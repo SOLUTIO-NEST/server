d0944e95cead56c5c345aa38e626c1b5ecae8541
JangYeongHu <jyhoo040912@gmail.com>
2026-01-19 21:56:17 +0900

    refactor: EmbeddedId를 통한 복합키 매핑

--- src/main/java/com/solutio/api/domain/applicant/repository/ApplicantRepository.java
+++ src/main/java/com/solutio/api/domain/applicant/repository/ApplicantRepository.java
@@ -1,12 +1,13 @@
 package com.solutio.api.domain.applicant.repository;
 
 import com.solutio.api.domain.applicant.domain.Applicant;
+import com.solutio.api.domain.applicant.domain.ApplicantId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ApplicantRepository extends JpaRepository<Applicant, String> {
-    List<Applicant> findByRecruitmentIdAndIsApprove(Long id, Boolean isApprove);
+public interface ApplicantRepository extends JpaRepository<Applicant, ApplicantId> {
+    List<Applicant> findByIdRecruitmentIdAndIsApprove(Long id, Boolean isApprove);
 
-    Applicant findByRecruitmentIdAndStudentId(Long id, String studentId);
+    Applicant findByIdRecruitmentIdAndIdStudentId(Long id, String studentId);
 }
