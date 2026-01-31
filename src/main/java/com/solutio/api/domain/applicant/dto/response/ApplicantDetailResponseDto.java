17df41e1d8141ba74e1f43a7d38cc688b9d21669
JangYeongHu <jyhoo040912@gmail.com>
2026-01-31 18:09:16 +0900

    feat: 신청자 상세 조회 API 구현

---
+++ src/main/java/com/solutio/api/domain/applicant/dto/response/ApplicantDetailResponseDto.java
@@ -0,0 +1,38 @@
+package com.solutio.api.domain.applicant.dto.response;
+
+import com.solutio.api.domain.applicant.domain.Applicant;
+import com.solutio.api.domain.member.domain.MainLanguage;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ApplicantDetailResponseDto {
+    private final String studentId;
+    private final String name;
+    private final String department;
+    private final String phoneNumber;
+    private final String email;
+    private final String bojId;
+    private final MainLanguage mainLanguage;
+    private final String applyReason;
+    private final Boolean isApprove;
+    private final LocalDateTime createdAt;
+
+    public static ApplicantDetailResponseDto from(Applicant applicant) {
+        return ApplicantDetailResponseDto.builder()
+                .studentId(applicant.getStudentId())
+                .name(applicant.getName())
+                .department(applicant.getDepartment())
+                .phoneNumber(applicant.getPhoneNumber())
+                .email(applicant.getEmail())
+                .bojId(applicant.getBojId())
+                .mainLanguage(applicant.getMainLanguage())
+                .applyReason(applicant.getApplyReason())
+                .isApprove(applicant.getIsApprove())
+                .createdAt(applicant.getCreatedAt())
+                .build();
+    }
+}
