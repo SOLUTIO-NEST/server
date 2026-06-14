f20528e74cd7d616920417287ed440001eafa53f
JangYeongHu <jyhoo040912@gmail.com>
2026-06-14 19:05:04 +0900

    fix: 인증 헤더 통일

--- src/main/java/com/solutio/api/domain/login/service/LoginService.java
+++ src/main/java/com/solutio/api/domain/login/service/LoginService.java
@@ -4,8 +4,6 @@ import com.solutio.api.domain.applicant.domain.Applicant;
 import com.solutio.api.domain.applicant.service.ApplicantService;
 import com.solutio.api.domain.login.domain.RefreshToken;
 import com.solutio.api.domain.login.dto.request.LoginRequestDto;
-import com.solutio.api.domain.login.dto.response.LoginResponse;
-import com.solutio.api.domain.login.dto.response.TokenHeader;
 import com.solutio.api.domain.login.dto.response.TokenInfo;
 import com.solutio.api.domain.login.repository.RefreshTokenRepository;
 import com.solutio.api.domain.member.domain.Member;
@@ -32,57 +30,50 @@ public class LoginService {
     private final TokenProvider tokenProvider;
     private final ApplicantService applicantService;
 
-    public LoginResponse login(LoginRequestDto request) {
+    public TokenInfo login(LoginRequestDto request) {
         String userId = request.getId();
         String password = request.getPassword();
         Member member = memberService.getUserById(userId);
         if (member != null) {
             member.isPasswordMatching(password, passwordEncoder);
-            String header;
-
-            header = TokenHeader.create(generateAndSaveToken(member)).toJson();
-            return LoginResponse.create(header, false);
+            return generateAndSaveToken(member);
         }
         Applicant applicant = applicantService.getApplicantById(userId);
         if (applicant != null) {
             applicant.isPasswordMatching(password, passwordEncoder);
-            String header;
-            header = TokenHeader.create(generateAndSaveToken(applicant)).toJson();
-            return LoginResponse.create(header, false);
+            return generateAndSaveToken(applicant);
         }
         throw new GeneralException(Status.ACCOUNT_NOT_FOUND);
     }
 
     private TokenInfo generateAndSaveToken(Member member) {
-
         String refreshToken = tokenProvider.generateToken(member.getStudentId(), Duration.ofDays(1), member.getRole().name());
         String accessToken = tokenProvider.generateToken(member.getStudentId(), Duration.ofHours(1), member.getRole().name());
         refreshTokenRepository.save(RefreshToken.of(member.getStudentId(), refreshToken));
-        return TokenInfo.create(refreshToken, accessToken);
+        return TokenInfo.create(accessToken, refreshToken);
     }
 
     private TokenInfo generateAndSaveToken(Applicant applicant) {
-
         String refreshToken = tokenProvider.generateToken(applicant.getStudentId(), Duration.ofDays(1), Role.GUEST.name());
         String accessToken = tokenProvider.generateToken(applicant.getStudentId(), Duration.ofHours(1), Role.GUEST.name());
         refreshTokenRepository.save(RefreshToken.of(applicant.getStudentId(), refreshToken));
-        return TokenInfo.create(refreshToken, accessToken);
+        return TokenInfo.create(accessToken, refreshToken);
     }
 
-    public TokenHeader reissueToken(HttpServletRequest request) {
+    public TokenInfo reissueToken(HttpServletRequest request) {
         String refreshToken = tokenProvider.resolveToken(request);
         RefreshToken refreshTokenEntity = refreshTokenRepository.findByRefreshToken(refreshToken)
-            .orElseThrow(() -> new GeneralException(Status.TOKEN_NOT_FOUND));;
+            .orElseThrow(() -> new GeneralException(Status.TOKEN_NOT_FOUND));
 
         refreshTokenRepository.delete(refreshTokenEntity);
 
         String userId = refreshTokenEntity.getUserId();
         Member member = memberService.getUserById(userId);
         if (member != null) {
-            return TokenHeader.create(generateAndSaveToken(member));
+            return generateAndSaveToken(member);
         }
 
         Applicant applicant = applicantService.getApplicantById(userId);
-        return TokenHeader.create(generateAndSaveToken(applicant));
+        return generateAndSaveToken(applicant);
     }
 }
