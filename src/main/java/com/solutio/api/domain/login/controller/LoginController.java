59cbd0c2aaad95164e313b85acffa3ca5e7a3819
YeongHu Jang <128132449+JangYeongHu@users.noreply.github.com>
2026-06-15 16:38:57 +0900

    fix: Security 설정 변경 완료 (#42)

--- src/main/java/com/solutio/api/domain/login/controller/LoginController.java
+++ src/main/java/com/solutio/api/domain/login/controller/LoginController.java
@@ -1,17 +1,15 @@
 package com.solutio.api.domain.login.controller;
 
 import com.solutio.api.domain.login.dto.request.LoginRequestDto;
-import com.solutio.api.domain.login.dto.response.LoginResponse;
-import com.solutio.api.domain.login.dto.response.TokenHeader;
+import com.solutio.api.domain.login.dto.response.TokenInfo;
 import com.solutio.api.domain.login.service.LoginService;
 import com.solutio.api.global.response.ApiResponse;
 import com.solutio.api.global.response.Status;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,42 +18,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/login")
+@RequiredArgsConstructor
 @Tag(name = "Login", description = "로그인")
 public class LoginController {
 
     private final LoginService loginService;
 
-    private final String authHeader;
-
-    public LoginController(
-        LoginService loginService,
-        @Value("${security.auth.header}") String authHeader) {
-
-        this.loginService = loginService;
-        this.authHeader = authHeader;
-    }
-
     @Operation(summary = "로그인", description = "ROLE_ANONYMOUS 권한이 필요함")
     @PostMapping("")
-    public ApiResponse<?> login(
-        HttpServletRequest request,
-        HttpServletResponse response,
+    public ApiResponse<TokenInfo> login(
         @Valid @RequestBody LoginRequestDto requestDto
     ) {
-        LoginResponse result = loginService.login(requestDto);
-        response.setHeader(authHeader, result.getHeader());
-        return ApiResponse.success(Status.OK.getCode(), Status.OK.getMessage(), result.getBody());
+        TokenInfo tokenInfo = loginService.login(requestDto);
+        return ApiResponse.success(Status.OK.getCode(), Status.OK.getMessage(), tokenInfo);
     }
 
     @Operation(summary = "[Guest] 토큰 재발급", description = "ROLE_GUEST 이상의 권한이 필요함")
     @PostMapping("/reissue")
     @PreAuthorize("hasRole('GUEST')")
-    public ApiResponse<Void> reissueToken(
-        HttpServletRequest request,
-        HttpServletResponse response
+    public ApiResponse<TokenInfo> reissueToken(
+        HttpServletRequest request
     ) {
-        TokenHeader headerData = loginService.reissueToken(request);
-        response.setHeader(authHeader, headerData.toJson());
-        return ApiResponse.success(Status.OK.getCode(), Status.OK.getMessage(), null);
+        TokenInfo tokenInfo = loginService.reissueToken(request);
+        return ApiResponse.success(Status.OK.getCode(), Status.OK.getMessage(), tokenInfo);
     }
 }
--- src/main/java/com/solutio/api/global/config/SecurityConfig.java
+++ src/main/java/com/solutio/api/global/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -35,7 +36,6 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-ui/**",
             "/api/v1/login",
-            "/test/**",
             "/actuator/health"
     };
 
@@ -56,6 +56,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET,ALLOWED_ALL_API_ENDPOINTS_GET).permitAll()
                         .requestMatchers(HttpMethod.POST,ALLOWED_ALL_API_ENDPOINTS_POST).permitAll()
                         .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
